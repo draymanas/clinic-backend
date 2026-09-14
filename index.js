6c0918acc1d5b53ec301e5e0935575b4d1a86cf7
draymanas <draymanas2016@gmail.com>
2026-09-14 23:47:02 +0300

    ???? تم تشغيل اشعار للمريض الساعه 10 صباحا في يوم الحجز للتذكير

--- index.js
+++ index.js
@@ -1352,7 +1352,7 @@ const patientRes = await pool.query('SELECT fcm_token FROM patients WHERE mobile
 // ⏰ ٢. نظام التذكير التلقائي اليومي بجدول المواعيد (الساعة 10:00 صباحاً بتوقيت مصر)
 // ==========================================================
 
-cron.schedule('10 10 * * *', async () => {
+cron.schedule('12 01* * *', async () => {
     console.log("--- ⏰ [cron] بدء فحص وإرسال تذكيرات المواعيد لليوم الحالي ---");
 
     const egyptDate = new Date().toLocaleString("en-CA", { timeZone: "Africa/Cairo" }).split(",")[0];
